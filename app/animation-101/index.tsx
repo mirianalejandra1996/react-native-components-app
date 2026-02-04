00df8eee0d71e42bb1ee31d5969c248a5bfbdc3d
mirian <mirianalejandra1996@gmail.com>
2026-02-03 22:50:00 -0500

    add bounce effect

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,16 +1,25 @@
 import ThemedButton from "@/presentation/shared/ThemedButton";
 import ThemedView from "@/presentation/shared/ThemedView";
 import { useRef } from "react";
-import { Animated } from "react-native";
+import { Animated, Easing } from "react-native";
 
 const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true,
+    }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      // easing: Easing.elastic(1), // Efecto
+      easing: Easing.bounce,
     }).start();
   };
 
@@ -19,7 +28,15 @@ const Animation101Screen = () => {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start();
+      // }).start(() => animatedTop.setValue(-100)); // Option 1
+    }).start(() => animatedTop.resetAnimation()); // Option 2
+
+    // Option 3
+    // Animated.timing(animatedTop, {
+    //   toValue: -100,
+    //   duration: 700,
+    //   useNativeDriver: true,
+    // }).start();
   };
 
   return (
@@ -30,6 +47,11 @@ const Animation101Screen = () => {
           width: 150,
           height: 150,
           opacity: animatedOpacity,
+          transform: [
+            {
+              translateY: animatedTop,
+            },
+          ],
         }}
       />
       <ThemedButton className="my-5" onPress={fadeIn}>
